a822f6136dfe81d607e115f6abf42ad0d1ce320c
YourUsername <youremail@example.com>
2025-10-01 12:01:52 +0700

    Logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from 'react'
 import { Routes, Route, Link, useLocation } from 'react-router-dom'
 import './navbar.css';
 import './App.css';
+import img1 from "./img/logo.png";
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Home from './pages/Home'
@@ -133,7 +134,7 @@ function UserMenu({ userChanged, setUserChanged }) {
 
     <div className="dropdown-item">
       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M208,32H184V24a8,8,0,0,0-16,0v8H88V24a8,8,0,0,0-16,0v8H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32ZM72,48v8a8,8,0,0,0,16,0V48h80v8a8,8,0,0,0,16,0V48h24V80H48V48ZM208,208H48V96H208V208Zm-48-56a8,8,0,0,1-8,8H136v16a8,8,0,0,1-16,0V160H104a8,8,0,0,1,0-16h16V128a8,8,0,0,1,16,0v16h16A8,8,0,0,1,160,152Z"></path></svg>
-      <Link to="/my-events" onClick={() => setOpen(false)} style={{ flex: 1, textDecoration: 'none', color: '#333' }}>
+      <Link to="/events/new" onClick={() => setOpen(false)} style={{ flex: 1, textDecoration: 'none', color: '#333' }}>
         Created Event
       </Link>
     </div>
@@ -156,7 +157,9 @@ export default function App() {
 
       <nav className='navbar'>
         <div className='navbar-logo'>
-          <Link to="/home">Logo</Link>
+          <Link to="/home">
+            <img src={img1} alt="Logo" style={{ height: '60px', width: '80px'}} />
+          </Link>
         </div>
 
         <div className='navbar-links'>
